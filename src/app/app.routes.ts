579978771a426f20aec8c5ca52773c0bb9144529
DieB219 <baquediego18@gmail.com>
2025-11-16 15:59:36 -0500

    Carga y ajuste de los componentes del login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    loadComponent: () => import('./components/login/login')
+  },
+  { path: 'acerca-de',
+    loadComponent: () => import('./components/aboutof/aboutof')
+  },
+  { path: '**', 
+    redirectTo: '' 
+  }
+];
